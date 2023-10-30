e29387accac3bcfefc49388c05df6e4fe50cc1c1
Agustin Carrizo <jcarrizoagustin@gmail.com>
2023-10-30 01:55:49 -0300

    Agregando formulario para carga de docente

--- apps/persona/forms.py
+++ apps/persona/forms.py
@@ -1,11 +1,36 @@
 from django.forms import ModelForm
+from django import forms
 from apps.persona.models import Alumno
+from .models import Docente
 
 
 # formulario para la clase alumno
 
 
 class AlumnoForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["nombre"].widget.attrs.update({"class": "form-control"})
+        self.fields["apellido"].widget.attrs.update({"class": "form-control"})
+        self.fields["dni"].widget.attrs.update({"class": "form-control"})
+        self.fields["matricula"].widget.attrs.update({"class": "form-control"})
+        self.fields["correoElectronico"].widget.attrs.update({"class": "form-control"})
     class Meta:
         model = Alumno
         fields = ('nombre', 'apellido', 'dni', 'matricula', 'correoElectronico')
+    
+
+# formulario para la clase Docente
+
+class DocenteForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["nombre"].widget.attrs.update({"class": "form-control"})
+        self.fields["apellido"].widget.attrs.update({"class": "form-control"})
+        self.fields["cuil"].widget.attrs.update({"class": "form-control"})
+    class Meta:
+        model = Docente
+        fields = ('nombre', 'apellido', 'cuil')
+        
+    
+        
--- apps/persona/views.py
+++ apps/persona/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib import messages
-from .forms import AlumnoForm
+from .forms import AlumnoForm, DocenteForm
 
 
 def registrar_alumno(request):
@@ -9,14 +9,22 @@ def registrar_alumno(request):
         registrar_alumno_form = AlumnoForm(request.POST)
         if registrar_alumno_form.is_valid():
             nuevo_alumno = registrar_alumno_form.save(commit=True)
-            messages.success(request, 'se ha registrado correctamente')
+            messages.success(request, f'se ha registrado el alumno {nuevo_alumno} correctamente')
     else:
         registrar_alumno_form = AlumnoForm()
     return render(request, 'persona/registroAlumnos.html', {'form': registrar_alumno_form})
 
 
 def registrar_docente(request):
-    return render(request, 'persona/registroDocente.html')
+    nuevo_docente = None
+    if request.method == 'POST':
+        registrar_docente_form = DocenteForm(request.POST)
+        if registrar_docente_form.is_valid():
+            nuevo_docente = registrar_docente_form.save(commit=True)
+            messages.success(request, f'se ha registrado el docente {nuevo_docente} correctamente')
+    else:
+        registrar_docente_form = DocenteForm()     
+    return render(request, 'persona/registroDocente.html',{'form': registrar_docente_form})
 
 
 def registrar_ascesor(request):
