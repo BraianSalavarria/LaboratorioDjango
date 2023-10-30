75c10a7c20f80486c85d3a2d4ad32548f8e7801c
BraianSalavarria <braian.salavarria@gmail.com>
2023-10-30 01:44:55 -0300

    se programo la vista darBaja

--- apps/persona/forms.py
+++ apps/persona/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from apps.persona.models import Alumno
 
 
@@ -9,3 +10,5 @@ class AlumnoForm(ModelForm):
     class Meta:
         model = Alumno
         fields = ('nombre', 'apellido', 'dni', 'matricula', 'correoElectronico')
+
+
--- apps/persona/views.py
+++ apps/persona/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from .forms import AlumnoForm
+from .models import Alumno
 
 
 def registrar_alumno(request):
@@ -23,6 +24,12 @@ def registrar_ascesor(request):
     return render(request, 'persona/registroAscesor.html')
 
 
-def dar_baja(request):
+def dar_baja_alumno(request):
+    mensaje = ''
+    if request.method == 'POST':
+        if 'dni' in request.POST:
+            alumno = get_object_or_404(Alumno, dni=request.POST['dni'])
+            alumno.delete()
+            mensaje = f'Operación exitosa'
 
-    return render(request, 'persona/darBaja.html')
+    return render(request, 'persona/darBaja.html', {'mensaje': mensaje})
